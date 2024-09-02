5bf4001d601807c53538039c77e93521cc161e14
Omar125Radwan <73297417+Omar125Radwan@users.noreply.github.com>
2024-09-02 03:55:00 +0200

    init

---
+++ sw_cached_pages.js
@@ -0,0 +1,17 @@
+const cacheName = 'version-1';
+
+const cacheAssets = [
+  '/index.html',
+  '/about.html',
+  '/css/style.css',
+  '/js/main.js'
+];
+
+// Call Install Event
+self.addEventListener('install', (e) => {
+  console.log('Service Worker: Installed');
+});
+// Call Activate Event
+self.addEventListener('activate', (e) => {
+  console.log('Service Worker: Activated');
+});
