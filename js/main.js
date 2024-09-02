c6f609c85feff4e14e1342d4ab73ccfe5d536f91
Omar125Radwan <73297417+Omar125Radwan@users.noreply.github.com>
2024-09-02 04:08:44 +0200

    done

--- js/main.js
+++ js/main.js
@@ -3,6 +3,5 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('../sw_cached_pages.js')
       .then(reg => console.log('Service Worker: Registered'))
       .catch(err => console.log(`Service Worker: Error: ${err}`));
-
   });
 }
